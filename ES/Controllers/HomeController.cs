f2c4338ec92a6530e02fc5080e91b95d833d8ce9
DESKTOP-C3S6S3K\Emanuel Tinoco <emanuel.g.tinoco@gmail.com>
2018-12-14 19:04:46 +0000

    LOGIN

--- ES/Controllers/HomeController.cs
+++ ES/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ES_TP_EF;
 
 namespace ES.Controllers
 {
@@ -10,7 +11,14 @@ namespace ES.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            if (Session["usuarioLogadoID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
 
         public ActionResult About()
@@ -25,6 +33,34 @@ namespace ES.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Utilizador u)
+        {
+            // esta action trata o post (login)
+            if (ModelState.IsValid) //verifica se é válido
+            {
+                using (estp2Entities dc = new estp2Entities())
+                {
+                    var v = dc.Utilizador.Where(a => a.username.Equals(u.username) && a.password.Equals(u.password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["usuarioLogadoID"] = v.id.ToString();
+                        Session["nomeUsuarioLogado"] = v.nome.ToString();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(u);
         }
     }
 }
